c87b30ceea46c28745382ee1531cddb80706abe6
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-25 15:54:50 -0700

    add routes for parties table as well as a app.put for the candidates table

--- server.js
+++ server.js
@@ -112,6 +112,87 @@ app.post('/api/candidate', ({ body }, res) => {
 });
 
 
+// route to allow for candidate changes (example: party affiliation)
+app.put('/api/candidate/:id', (req, res) => {
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
+
+// route for 'parties' table
+app.get('/api/party', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  
+  
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// route for single party from 'parties'
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+
+// delete route for 'parties' table
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+
+
 
 // Default response for any other request(Not Found) Catch all
 // needs to be last route
